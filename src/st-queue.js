3f1dfe4cd545cf72ffbae8270ea6b73a20fe550d
AlexGrin1 <a_grynevich@mail.ru>
2021-10-19 00:50:45 +0300

    refactor : solved queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.linkedList = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.linkedList;
+  }
+
+  enqueue(value) {
+    let node = new ListNode(value);
+    if (this.linkedList === null) {
+      this.linkedList = node;
+    } else {
+      let actual = this.linkedList;
+      while (actual.next) {
+        actual = actual.next;
+      }
+      actual.next = node;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let value = this.linkedList.value;
+    this.linkedList = this.linkedList.next;
+    return value;
   }
-
-}
+};
